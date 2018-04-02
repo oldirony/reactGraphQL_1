f11eb52f945aac70151d30b829d933fd519d058d
oldirony <raffaele.abramini@gmail.com>
2018-04-02 12:52:50 +0100

    Add Songlist component

---
+++ client/components/SongList.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
+
+class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map((song) => (
+      <li key={song.id}
+      className="collection-item">
+        {song.title}
+      </li>
+    ))
+  }
+  render() {
+    const { songs, loading } = this.props.data;
+
+    if (loading) return <div>Loading data...</div>
+    if (!songs) return <div>No songs added yet.</div>
+
+    return (
+      <div>
+        <h1>List of songs</h1>
+        <hr/>
+        <ul className="collection">
+          {this.renderSongs()}
+        </ul>
+      </div>
+    )
+  }
+}
+
+const query = gql`
+  {
+    songs {
+      id
+      title
+    }
+  }
+`
+
+export default graphql(query)(SongList)
--- client/index.js
+++ client/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ApolloClient from 'apollo-client'
+import {ApolloProvider} from 'react-apollo'
+import SongList from './components/SongList'
 
-const Root = () => {
-  return <div>Lyrical</div>
-};
+const client = new ApolloClient({})
+
+const Root = () => (
+  <ApolloProvider
+    client={client}>
+    <SongList />
+  </ApolloProvider>
+)
 
 ReactDOM.render(
-  <Root />,
+  <Root/>,
   document.querySelector('#root')
-);
+)
